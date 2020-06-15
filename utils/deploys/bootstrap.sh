a7e641c4eacedfc8db22dd79415ed2435baeb80c
payne911 <jeremi.grenier-berthiaume@umontreal.ca>
2020-06-15 02:01:21 -0400

    new run config for duo launch

--- utils/deploys/bootstrap.sh
+++ utils/deploys/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 echo "========== LAUNCHING SERVER APP ========="
-apk add --no-cache libstdc++  # get a missing library
-java --enable-preview -jar /app.jar
+#apk add --no-cache libstdc++  # get a missing library
+#java --enable-preview -jar /app.jar
+chmod +x server
+./server
