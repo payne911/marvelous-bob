6816621b4bd2e5ec1b15c0288b689b328162e3cf
payne911 <payne911@users.noreply.github.com>
2020-06-15 16:52:22 -0400

    testing CI/CD with new Image size

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 #FROM openjdk:14-jdk-alpine
-#FROM alpine:3.7
-FROM ubuntu:latest
-#RUN apk add --no-cache libc6-compat libstdc++
+FROM alpine:3.7
+#FROM ubuntu:latest
 COPY server/build/jpackage/server /server
 COPY utils/deploys/bootstrap.sh /bootstrap.sh
 RUN chmod +x /bootstrap.sh
--- utils/deploys/bootstrap.sh
+++ utils/deploys/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 echo "========== LAUNCHING SERVER APP ========="
-#apk add --no-cache libstdc++ libc6-compat
-#apk add --no-cache libstdc++  # get a  missing library
-#java --enable-preview -jar /app.jar
 cd /server/bin
 chmod +x server
-./server
+ldd /server
+sudo ./server
