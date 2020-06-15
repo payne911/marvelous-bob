e4e34b2b420405921641a9e6ea84f275483c4480
payne911 <payne911@users.noreply.github.com>
2020-06-15 16:29:43 -0400

    testing CI/CD with new Image size

--- utils/deploys/bootstrap.sh
+++ utils/deploys/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 echo "========== LAUNCHING SERVER APP ========="
+#apk add --no-cache libstdc++ libc6-compat
 #apk add --no-cache libstdc++  # get a  missing library
 #java --enable-preview -jar /app.jar
-cd /server/bin
+#cd /server/bin
 chmod +x server
-echo .
-apk add --no-cache libstdc++
-apk add --no-cache libc6-compat
-./server
+./server/bin/server
