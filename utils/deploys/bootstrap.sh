3768c726b71c4ca96c3921095f225af848dca845
payne911 <payne911@users.noreply.github.com>
2020-06-15 15:18:29 -0400

    testing CI/CD with new Image size

--- utils/deploys/bootstrap.sh
+++ utils/deploys/bootstrap.sh
@@ -2,6 +2,10 @@
 echo "========== LAUNCHING SERVER APP ========="
 #apk add --no-cache libstdc++  # get a missing library
 #java --enable-preview -jar /app.jar
+cd / && ls -la *
+echo "1"
+cd server && ls -la *
+echo "2"
 cd /server/bin
 chmod +x server
 ./server
