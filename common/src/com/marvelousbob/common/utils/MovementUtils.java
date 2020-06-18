e474d617ac1296a72ba24840e41f6a25fa556dad
payne911 <payne911@users.noreply.github.com>
2020-06-18 00:06:31 -0400

    Optimized interpolation

--- common/src/com/marvelousbob/common/utils/MovementUtils.java
+++ common/src/com/marvelousbob/common/utils/MovementUtils.java
@@ -3,8 +3,15 @@ package com.marvelousbob.common.utils;
 import com.badlogic.gdx.math.Interpolation;
 import com.marvelousbob.common.network.register.dto.GameStateDto;
 import com.marvelousbob.common.network.register.dto.PlayerDto;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MovementUtils {
+
+    /**
+     * Optimisation to prevent interpolation from trying to calculate unnoticeable differences.
+     */
+    private static final float SMALLEST_DELTA = 0.5f;
 
     private MovementUtils() {
     }
@@ -13,9 +20,33 @@ public class MovementUtils {
         game.getPlayerDtos().forEach(p -> interpolatePlayer(p, delta));
     }
 
+    /**
+     * @param p     a player to interpolate
+     * @param delta the amount of time which has past since last render
+     * @see <a href="https://github.com/libgdx/libgdx/wiki/Interpolation">libGDX Interpolation Wiki
+     * Page</a>
+     */
     public static void interpolatePlayer(PlayerDto p, float delta) {
-        // https://github.com/libgdx/libgdx/wiki/Interpolation
-        p.setCurrX(Interpolation.exp10Out.apply(p.getCurrX(), p.getDestX(), delta));
-        p.setCurrY(Interpolation.exp10Out.apply(p.getCurrY(), p.getDestY(), delta));
+
+        if (wantsToMove(p)) {
+            log.info("Interpolating player UUID %s from (%f , %f) to (%f , %f)"
+                    .formatted(p.getUuid().getStringId(), p.getCurrX(), p.getCurrY(), p.getDestX(),
+                            p.getDestY()));
+
+            p.setCurrX(isBigEnoughDelta(p.getCurrX(), p.getDestX())
+                    ? Interpolation.exp10Out.apply(p.getCurrX(), p.getDestX(), delta)
+                    : p.getDestX());
+            p.setCurrY(isBigEnoughDelta(p.getCurrY(), p.getDestY())
+                    ? Interpolation.exp10Out.apply(p.getCurrY(), p.getDestY(), delta)
+                    : p.getDestY());
+        }
+    }
+
+    private static boolean isBigEnoughDelta(float currX, float destX) {
+        return Math.abs(currX - destX) > SMALLEST_DELTA;
+    }
+
+    private static boolean wantsToMove(PlayerDto p) {
+        return p.getCurrX() != p.getDestX() || p.getCurrY() != p.getDestY();
     }
 }
