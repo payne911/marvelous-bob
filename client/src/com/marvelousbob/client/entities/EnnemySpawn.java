3dfd92b37f3335476755b54d27f0f1b048376032
Applin <lapplin.olivier@gmail.com>
2020-06-24 15:23:39 -0400

    star shape ennemy spawn

--- client/src/com/marvelousbob/client/entities/EnnemySpawn.java
+++ client/src/com/marvelousbob/client/entities/EnnemySpawn.java
@@ -19,14 +19,18 @@ public class EnnemySpawn implements Drawable {
         Vector2 p1 = new Vector2(center.x + size, center.y);
         Vector2 p2 = p1.cpy().rotateAround(center, 120);
         Vector2 p3 = p2.cpy().rotateAround(center, 120);
-        Polygon triangle1 = new Polygon(new float[]{p1.x, p1.y, p2.x, p2.y, p3.x, p3.y});
+        float[] t1vertices = new float[]{p1.x, p1.y, p2.x, p2.y, p3.x, p3.y};
         Polygon triangle2 = new Polygon(new float[]{p1.x, p1.y, p2.x, p2.y, p3.x, p3.y});
         triangle2.rotate(180);
-        float[] t2Vertices = triangle2.getVertices();
+        float[] t2Vertices = triangle2.getTransformedVertices();
         float[] starVertices = new float[12];
-        // todo fill star verticves
+        for (int i = 0; i < t1vertices.length; i++) {
+            starVertices[i] = t1vertices[i];
+        }
+        for (int i = 0; i < t2Vertices.length; i++) {
+            starVertices[i + t1vertices.length] = t2Vertices[i];
+        }
         return new EnnemySpawn(new Polygon(starVertices), color);
-
     }
 
     @Override
