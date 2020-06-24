b1782daea27f8f21560586ae2d0472cdce1ef5bb
Applin <lapplin.olivier@gmail.com>
2020-06-24 16:03:52 -0400

    testing stuff

--- client/src/com/marvelousbob/client/entities/Level.java
+++ client/src/com/marvelousbob/client/entities/Level.java
@@ -1,25 +1,29 @@
 package com.marvelousbob.client.entities;
 
 import com.badlogic.gdx.math.Intersector;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lombok.Data;
 import space.earlygrey.shapedrawer.ShapeDrawer;
+
+import java.util.*;
 
 @Data
 public class Level implements Drawable {
 
     private final Intersector intersector;
-    private final Base base;
+
+    private final List<Base> base;
+    private final List<EnemySpawn> enemySpawns;
     private final List<Wall> walls;
     private List<Player> players = new ArrayList<>();
-    private Set<Ennemy> ennemies = new HashSet<>();
+    private Set<Ennemy> ennemies = Collections.newSetFromMap(new IdentityHashMap<>());
 
     @Override
     public void drawMe(ShapeDrawer shapeDrawer) {
+        base.forEach(b -> b.drawMe(shapeDrawer));
         walls.forEach(w -> w.drawMe(shapeDrawer));
         players.forEach(p -> p.drawMe(shapeDrawer));
+        ennemies.forEach(e -> e.drawMe(shapeDrawer));
+        enemySpawns.forEach(es -> es.drawMe(shapeDrawer));
     }
+
 }
