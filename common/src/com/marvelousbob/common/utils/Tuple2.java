7b09ab2137d01b9dca885e60540a8e472af7597d
payne911 <jeremi.grenier-berthiaume@umontreal.ca>
2020-07-01 15:40:54 -0400

    Tuple utils

---
+++ common/src/com/marvelousbob/common/utils/Tuple2.java
@@ -0,0 +1,12 @@
+package com.marvelousbob.common.utils;
+
+public class Tuple2<T, U> {
+
+    public final T first;
+    public final U second;
+
+    public Tuple2(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+}
